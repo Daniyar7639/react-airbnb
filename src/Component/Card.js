1dc964127941dc9bf7882852bd76c3fa979de448
Daniyar <dkurbanov7639@gmail.com>
2023-02-28 09:57:51 +0500

    use spread operator to pass props

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -4,26 +4,26 @@ import star1 from "../images/Star1.png"
 export default function Card (props) {
     
     let badgeText
-    if(props.d.openSpots === 0) {
+    if(props.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.d.location === "Online") {
+    } else if (props.location === "Online") {
         badgeText = "ONLINE"
     }
     return (
         <div className="card">
             <div className="card-img">
                 {badgeText && <div className="card-badge">{badgeText}</div>}
-                <img className="card-img1" src={require(`..//images/${props.d.coverImg}`)} alt="img" />
+                <img className="card-img1" src={require(`..//images/${props.coverImg}`)} alt="img" />
             </div>
             <div className="card-stats">
                 <img className="card-star" src={star1} alt="img"/>
-                <span className="card-stat">{props.d.stats.rating}</span>
-                <span className="card-stat">({props.d.stats.reviewCount})</span>
+                <span className="card-stat">{props.stats.rating}</span>
+                <span className="card-stat">({props.stats.reviewCount})</span>
                 <span className="card-stat">.</span>
-                <span className="card-stat">{props.d.location}</span>
+                <span className="card-stat">{props.location}</span>
             </div>
-            <p className="card-title">{props.d.title}</p>
-            <p> <small className="bold">From ${props.d.price}</small> / person</p>
+            <p className="card-title">{props.title}</p>
+            <p> <small className="bold">From ${props.price}</small> / person</p>
         </div>
     )
 }
